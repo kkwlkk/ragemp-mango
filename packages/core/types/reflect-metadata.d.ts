0a9abe8cf5ae45a3af2a92f3f82672ff6142ad55
wlk <wlk78380@gmail.com>
2025-12-30 19:59:28 +0100

    fix: type

--- packages/core/types/reflect-metadata.d.ts
+++ packages/core/types/reflect-metadata.d.ts
@@ -1,6 +1,6 @@
 declare namespace Reflect {
     function getMetadata<T = unknown>(metadataKey: unknown, target: object): T | undefined;
-    function getMetadata<T = unknown>(metadataKey: unknown, target: object, propertyKey: string | symbol): T | undefined;
+    function getMetadata<T = unknown>(metadataKey: unknown, target: object, propertyKey?: string | symbol): T | undefined;
     function defineMetadata<T = unknown>(metadataKey: unknown, metadataValue: T, target: object): void;
-    function defineMetadata<T = unknown>(metadataKey: unknown, metadataValue: T, target: object, propertyKey: string | symbol): void;
+    function defineMetadata<T = unknown>(metadataKey: unknown, metadataValue: T, target: object, propertyKey?: string | symbol): void;
 }
